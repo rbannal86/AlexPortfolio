15054d4674a1eec453237d5afdfb27893f6e131f
Alex Bannow <rbannal@gmail.com>
2020-01-30 16:04:02 -0500

    added image for tablet and up

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,9 +4,12 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="header__main">
-      <h1>Alex Bannow.</h1>
-      <h2>Full Stack Web Development.</h2>
-      <h3>Hello there.</h3>
+      <div className="header__text">
+        <h1>Alex Bannow.</h1>
+        <h2>Full Stack Web Development.</h2>
+        <h3>Hello there.</h3>
+      </div>
+      <img className="header__self" src="Images/self.jpg" alt="Alex" />
     </header>
   );
 };
