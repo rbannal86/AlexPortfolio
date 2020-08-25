e1652aa834d098a8f3ae7ad8e1a435f9a7005e73
Alex Bannow <rbannal@gmail.com>
2020-08-25 16:22:56 -0400

    removed two projects, added a new one

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -8,175 +8,149 @@ const Projects = () => {
     {
       original: "Images/bp1.png",
       description: "Landing Page for New Users",
-      originalAlt: "Landing Page"
+      originalAlt: "Landing Page",
     },
     {
       original: "Images/bp2.png",
       description: "Information Page",
-      originalAlt: "Information Page"
+      originalAlt: "Information Page",
     },
     {
       original: "Images/bp3.png",
       description: "Registration Page",
-      originalAlt: "Registration Page"
+      originalAlt: "Registration Page",
     },
     {
       original: "Images/bp4.png",
       description: "Landing Page for Returning Users",
-      originalAlt: "Landing Page for returning users"
+      originalAlt: "Landing Page for returning users",
     },
     {
       original: "Images/bp5.png",
       description: "Letter Writing Page",
-      originalAlt: "Letter Writing Page"
+      originalAlt: "Letter Writing Page",
     },
     {
       original: "Images/bp6.png",
       description: "First Letter Description Page",
-      originalAlt: "First Letter Description Page"
+      originalAlt: "First Letter Description Page",
     },
     {
       original: "Images/bp7.png",
       description: "User Dashboard",
-      originalAlt: "User Dashboard"
+      originalAlt: "User Dashboard",
     },
     {
       original: "Images/bp8.png",
       description: "Conversation Page",
-      originalAlt: "Conversation Page"
+      originalAlt: "Conversation Page",
     },
     {
       original: "Images/bp9.png",
       description: "Conversation Details Page",
-      originalAlt: "Conversation Details Page"
+      originalAlt: "Conversation Details Page",
     },
     {
       original: "Images/bp10.png",
       description: "Letter Page",
-      originalAlt: "Letter Page"
+      originalAlt: "Letter Page",
     },
     {
       original: "Images/bp11.png",
       description: "Received Letter Page",
-      originalAlt: "Received Letter Page"
+      originalAlt: "Received Letter Page",
     },
     {
       original: "Images/bp12.png",
       description: "Conversation History Page",
-      originalAlt: "Conversation History Page"
-    }
+      originalAlt: "Conversation History Page",
+    },
   ];
 
   const mlImages = [
     {
       original: "Images/ml1.png",
       description: "Landing Page",
-      originalAlt: "Landing Page"
+      originalAlt: "Landing Page",
     },
     {
       original: "Images/ml2.png",
       description: "Registration Page",
-      originalAlt: "Registration Page"
+      originalAlt: "Registration Page",
     },
     {
       original: "Images/ml3.png",
       description: "Login Page",
-      originalAlt: "Login Page"
+      originalAlt: "Login Page",
     },
     {
       original: "Images/ml4.png",
       description: "User Dashboard",
-      originalAlt: "User Dashboard"
+      originalAlt: "User Dashboard",
     },
     {
       original: "Images/ml5.png",
       description: "Employee Profile Page",
-      originalAlt: "Employee Profile Page"
+      originalAlt: "Employee Profile Page",
     },
     {
       original: "Images/ml6.png",
       description: "Create Project Page",
-      originalAlt: "Create Project Page"
+      originalAlt: "Create Project Page",
     },
     {
       original: "Images/ml7.png",
       description: "Project Page",
-      originalAlt: "Project Page"
+      originalAlt: "Project Page",
     },
     {
       original: "Images/ml8.png",
       description: "Add Job Form",
-      originalAlt: "Add Job Form"
+      originalAlt: "Add Job Form",
     },
     {
       original: "Images/ml9.png",
       description: "Edit Project Form",
-      originalAlt: "Edit Project Form"
+      originalAlt: "Edit Project Form",
     },
     {
       original: "Images/ml10.png",
       description: "Edit Job Form",
-      originalAlt: "Edit Job Form"
+      originalAlt: "Edit Job Form",
     },
     {
       original: "Images/ml11.png",
       description: "Log Hours Form",
-      originalAlt: "Log Hours Form"
+      originalAlt: "Log Hours Form",
     },
     {
       original: "Images/ml12.png",
       description: "Catch-All Page for Errors",
-      originalAlt: "Catch-All Page for Errors"
-    }
+      originalAlt: "Catch-All Page for Errors",
+    },
   ];
 
-  const petsImages = [
+  const tbImages = [
     {
-      original: "Images/pet1.png",
-      description: "Adoption Page",
-      originalAlt: "Adoption Page"
+      original: "Images/tb2.png",
+      description: "Main Screen",
+      originalAlt: "Main Screen",
     },
     {
-      original: "Images/pet2.png",
-      description: "Available Pets Page",
-      originalAlt: "Available Pets Page"
+      original: "Images/tb1.png",
+      description: "First Search Results",
+      originalAlt: "First Search Results",
     },
     {
-      original: "Images/pet3.png",
-      description: "Success Stories Page",
-      originalAlt: "Success Stories Page"
-    }
-  ];
-
-  const srImages = [
-    {
-      original: "Images/sr1.png",
-      description: "Registration Page",
-      originalAlt: "Registration Page"
+      original: "Images/tb3.png",
+      description: "Empty Search Results",
+      originalAlt: "Empty Search Results",
     },
     {
-      original: "Images/sr2.png",
-      description: "Login Page",
-      originalAlt: "Login Page"
+      original: "Images/tb4.png",
+      description: "Search Results for Chaucer",
+      originalAlt: "Search Results for Chaucer",
     },
-    {
-      original: "Images/sr3.png",
-      description: "User Dashboard",
-      originalAlt: "User Dashboard"
-    },
-    {
-      original: "Images/sr4.png",
-      description: "Translation Page",
-      originalAlt: "Translation Page"
-    },
-    {
-      original: "Images/sr5.png",
-      description: "Result Page for Correct Answer"
-    },
-    {
-      original: "Images/sr6.png",
-      description: "Result Page for Incorrect Answer"
-    }
   ];
 
   return (
@@ -303,24 +277,18 @@ const Projects = () => {
           </div>
         </li>
         <li className="projects__list_item">
-          <h3 className="projects__project_title">Petful</h3>
+          <h3 className="projects__project_title">10Books</h3>
           <div className="projects__project_description">
             <p>
-              The Petful app is a first-in, first-out adoption site designed to
-              shake up the pet adoption process. Users are promoted to join a
-              queue for adoption. Once they move to the front of the line, they
-              are then given the choice of adopting a cat, a dog, both, or none.
-              If they choose to adopt neither, they are moved to the back of the
-              line. This app is aimed at people who don't like the typical
-              process of going to an animal shelter and want a little bit of
-              chance added to the adoption experience.
+              10Books is a simple book search app that utilizes the GoogleBooks
+              API and a custom-made Firebase database as a cache. The app will
+              return up to ten books that match the search query and present the
+              results with all of the relevant and available information.
             </p>
             <p>
-              This app was an exercise in building off of an existing codebase,
-              implementing all of the processes for the queue system, and
-              tailoring the features to a very specific set of user stories.
-              Beyond these challenges, the idea for the project was to implement
-              all of the functionality and styling in a single day.
+              This app was my first attempt at a FERN stack and building a
+              cache. The styling was also driven by accessibility standards like
+              color contrast and text size.
             </p>
           </div>
           <div className="projects__project_stack">
@@ -338,8 +306,8 @@ const Projects = () => {
               alt="react shield"
             />
             <img
-              src="https://img.shields.io/badge/PostgreSQL-purple"
-              alt="postgres shield"
+              src="https://img.shields.io/badge/Firebase-orange"
+              alt="firebase shield"
             />
             <img
               src="https://img.shields.io/badge/Node.js-lightgreen"
@@ -347,85 +315,16 @@ const Projects = () => {
             />
           </div>
           <div className="projects__project_screenshots">
-            <ImageGallery items={petsImages} showThumbnails={false} />
+            <ImageGallery items={tbImages} showThumbnails={false} />
           </div>
           <div className="projects__project_links">
-            <a
-              href="https://github.com/thinkful-ei-heron/petful_client_alex_shannon"
-              target="blank"
-            >
+            <a href="https://github.com/rbannal86/10BooksClient" target="blank">
               Client GitHub Repo
             </a>
-            <a
-              href="https://github.com/thinkful-ei-heron/petful_server_alex_shannon"
-              target="blank"
-            >
+            <a href="https://github.com/rbannal86/10BooksServer" target="blank">
               Server GitHub Repo
             </a>
-            <a href="https://petful-client.rbannal.now.sh/" target="blank">
-              Live App
-            </a>
-          </div>
-        </li>
-        <li className="projects__list_item">
-          <h3 className="projects__project_title">Spaced Repetition</h3>
-          <div className="projects__project_description">
-            <p>
-              The Spaced Repetition App is a language learning tool designed
-              around the concept of using flashcards and an algorithm that helps
-              improve the rate at which a person learns. The app is for people
-              who want to learn language at their own pace on any available
-              device.
-            </p>
-            <p>
-              This app was developed as an exercise in building on top of an
-              existing code base, design, and using data structure theory in a
-              practical setting.
-            </p>
-          </div>
-          <div className="projects__project_stack">
-            <img
-              src="https://img.shields.io/badge/JavaScript-yellow"
-              alt="javascript shield"
-            />
-            <img src="https://img.shields.io/badge/CSS-blue" alt="css shield" />
-            <img
-              src="https://img.shields.io/badge/HTML-red"
-              alt="html shield"
-            />
-            <img
-              src="https://img.shields.io/badge/React-lightblue"
-              alt="react shield"
-            />
-            <img
-              src="https://img.shields.io/badge/PostgreSQL-purple"
-              alt="postgres shield"
-            />
-            <img
-              src="https://img.shields.io/badge/Node.js-lightgreen"
-              alt="node shield"
-            />
-          </div>
-          <div className="projects__project_screenshots">
-            <ImageGallery items={srImages} showThumbnails={false} />
-          </div>
-          <div className="projects__project_links">
-            <a
-              href="https://github.com/thinkful-ei-heron/spaced-repetition-client-alex-sean"
-              target="blank"
-            >
-              Client GitHub Repo
-            </a>
-            <a
-              href="https://github.com/thinkful-ei-heron/spaced-repetition-api-alex-sean"
-              target="blank"
-            >
-              Server GitHub Repo
-            </a>
-            <a
-              href="https://spaced-repetition-client.rbannal.now.sh/"
-              target="blank"
-            >
+            <a href="https://https://10books.vercel.app/" target="blank">
               Live App
             </a>
           </div>
